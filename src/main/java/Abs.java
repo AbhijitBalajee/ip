5fc2b3360d97da042e698d79258b0ce41c584969
AbhijitBalajee <abhijitbalajee@gmail.com>
2026-01-29 22:43:17 +0800

    Level 3 Completed

--- src/main/java/Abs.java
+++ src/main/java/Abs.java
@@ -5,35 +5,52 @@ public class Abs {
         String indent = "    ";
         String separator = indent + "-------------------------------------------";
         String logo = indent + "  █████╗ ██████╗ ███████╗\n"
-                    + indent + " ██╔══██╗██╔══██╗██╔════╝\n"
-                    + indent + " ███████║██████╔╝███████╗\n"
-                    + indent + " ██╔══██║██╔══██╗╚════██║\n"
-                    + indent + " ██║  ██║██████╔╝███████║\n"
-                    + indent + " ╚═╝  ╚═╝╚═════╝ ╚══════╝\n";
+                + indent + " ██╔══██╗██╔══██╗██╔════╝\n"
+                + indent + " ███████║██████╔╝███████╗\n"
+                + indent + " ██╔══██║██╔══██╗╚════██║\n"
+                + indent + " ██║  ██║██████╔╝███████║\n"
+                + indent + " ╚═╝  ╚═╝╚═════╝ ╚══════╝\n";
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         System.out.println(separator);
         System.out.println(logo);
         System.out.println(indent + "Hellooo! I'm Abs!");
-        System.out.println(indent + "What can I do for you?");
+        System.out.println(indent + "What is your name?");
         System.out.println(separator);
 
         Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();  // Get the user's name
+
+        System.out.println(separator);
+        System.out.println(indent + "Hi " + userName + "! Hope you are doing great!");
+        System.out.println(indent + "What can I do for you?");
+        System.out.println(separator);
+
         String input = scanner.nextLine();
 
         while (!input.equals("bye")) {
             System.out.println(separator);
 
             if (input.equals("list")) {
-
+                System.out.println(indent + "Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(indent + (i + 1) + ". " + tasks[i]);
+                    System.out.println(indent + (i + 1) + "." + tasks[i]);
                 }
+            } else if (input.startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(input.substring(5));
+                tasks[taskNumber - 1].markAsDone();
+                System.out.println(indent + "Nice! I've marked this task as done:");
+                System.out.println(indent + "  " + tasks[taskNumber - 1]);
+            } else if (input.startsWith("unmark ")) {
+                int taskNumber = Integer.parseInt(input.substring(7));
+                tasks[taskNumber - 1].markAsNotDone();
+                System.out.println(indent + "OK, I've marked this task as not done yet:");
+                System.out.println(indent + "  " + tasks[taskNumber - 1]);
             } else {
-
-                tasks[taskCount] = input;
+                Task newTask = new Task(input);
+                tasks[taskCount] = newTask;
                 taskCount++;
                 System.out.println(indent + "added: " + input);
             }
@@ -43,7 +60,8 @@ public class Abs {
         }
 
         System.out.println(separator);
-        System.out.println(indent + "Byeee! Hope to see you again realll soonnn!");
+        System.out.println(indent + "Bye " + userName + "!");
+        System.out.println(indent + "Hope to see you again realll soonnn!");
         System.out.println(separator);
     }
 }
