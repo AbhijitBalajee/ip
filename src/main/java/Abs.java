481278fccfd0eb11e1b55be53adcf8ad16cb5c7d
AbhijitBalajee <abhijitbalajee@gmail.com>
2026-01-29 22:07:58 +0800

    Level 1 Completed

--- src/main/java/Abs.java
+++ src/main/java/Abs.java
@@ -1,21 +1,34 @@
+import java.util.Scanner;
+
 public class Abs {
     public static void main(String[] args) {
-        String separator = "----------------------------------------------";
-        String logo = "  █████╗ ██████╗ ███████╗\n"
-                + " ██╔══██╗██╔══██╗██╔════╝\n"
-                + " ███████║██████╔╝███████╗\n"
-                + " ██╔══██║██╔══██╗╚════██║\n"
-                + " ██║  ██║██████╔╝███████║\n"
-                + " ╚═╝  ╚═╝╚═════╝ ╚══════╝\n";
+        String indent = "    ";
+        String separator = indent + "-------------------------------------------";
+        String logo = indent + "  █████╗ ██████╗ ███████╗\n"
+                    + indent + " ██╔══██╗██╔══██╗██╔════╝\n"
+                    + indent + " ███████║██████╔╝███████╗\n"
+                    + indent + " ██╔══██║██╔══██╗╚════██║\n"
+                    + indent + " ██║  ██║██████╔╝███████║\n"
+                    + indent + " ╚═╝  ╚═╝╚═════╝ ╚══════╝\n";
 
         System.out.println(separator);
         System.out.println(logo);
-        System.out.println("Hellooo! I'm Abs!");
-        System.out.println("What can I do for you?");
+        System.out.println(indent + "Hellooo! I'm Abs!");
+        System.out.println(indent + "What can I do for you?");
         System.out.println(separator);
 
-        System.out.println("Byeee! Hope to see you again realll sooooonnn!");
-        System.out.println(separator);
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
 
+        while (!input.equals("bye")) {
+            System.out.println(separator);
+            System.out.println(indent + input);
+            System.out.println(separator);
+            input = scanner.nextLine();
+        }
+
+        System.out.println(separator);
+        System.out.println(indent + "Byeee. Hope to see you again realll soonnn!");
+        System.out.println(separator);
     }
 }
